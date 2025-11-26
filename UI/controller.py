ecdcaefcd6831b3b2140d1a65a982b490c8a3af6
FedeCiocio05 <ciociola.federico@gmail.com>
2025-11-26 17:07:29 +0100

    Commit di Lab; Model + 1/2 controller

--- UI/controller.py
+++ UI/controller.py
@@ -16,5 +16,30 @@ class Controller:
         * Numero di Tratte
         * Lista di Tratte che superano il costo indicato come soglia
         """
+        # 1. Recupero input dalla View
+        input_str = self._view.guadagno_medio_minimo.value
+
+        # 2. Validazione
+        try:
+            if input_str == "":
+                raise ValueError  # Gestisco stringa vuota come errore
+            threshold = float(input_str)
+        except ValueError:
+            self._view.show_alert("Errore: Inserire un valore numerico valido per il guadagno medio!")
+            self._view.update()
+            return
+
+        # 3. Costruzione Grafo tramite Model
+        # Questo metodo resetta il grafo e lo ricrea con i filtri
+        self._model.costruisci_grafo(threshold)
+
+        # 4. Aggiornamento UI
+        self._view.lista_visualizzazione.controls.clear()
+
+        # Recupero statistiche dal Model
+        n_nodi = self._model.get_num_nodes()
+        n_archi = self._model.get_num_edges()
+
+
         # TODO
 
--- model/model.py
+++ model/model.py
@@ -1,4 +1,4 @@
-from database.dao import DAO
+from database.dao import dao
 import networkx as nx
 
 class Model:
@@ -6,12 +6,53 @@ class Model:
         self._nodes = None
         self._edges = None
         self.G = nx.Graph()
+        # Mappe per accesso rapido (id -> oggetto)
+        self._id_map = {}
 
     def costruisci_grafo(self, threshold):
         """
         Costruisce il grafo (self.G) inserendo tutti gli Hub (i nodi) presenti e filtrando le Tratte con
         guadagno medio per spedizione >= threshold (euro)
         """
+        # 1. Pulizia: Se il grafo esiste già, lo svuoto
+        self.G.clear()
+        self._nodes = set()
+        self._edges = set()
+
+        # 2. Recupero tutti gli Hub dal DB
+        all_hubs = dao.get_hub()
+
+        # 3. Aggiungo i nodi al grafo
+        for hub in all_hubs:
+            # Salvo nella mappa per recuperare l'oggetto dato l'ID
+            self._id_map[hub.id_hub] = hub
+            self._nodes.add(hub)
+
+            # Aggiungo il nodo al grafo (passo l'intero oggetto)
+            self.G.add_node(hub)
+
+        # 4. Recupero tutte le tratte (archi potenziali)
+        all_tratte = dao.get_tratta()  # Metodo corretto nel turno precedente
+
+        # 5. Aggiungo gli archi filtrando
+        for tratta in all_tratte:
+            # Calcolo guadagno medio: richiamo metodo della classe tratta
+            if tratta.n_spedizioni > 0:
+                peso = tratta.peso_tratta()
+            else:
+                peso = 0
+
+        # FILTRO: Aggiungo l'arco solo se la media supera la soglia
+            if peso > float(threshold):
+                # Recupero gli oggetti nodo reali usando la mappa
+                u = self._id_map[tratta.h1]
+                v = self._id_map[tratta.h2]
+
+                # Aggiungo l'arco
+                self.G.add_edge(u, v, weight=peso)
+
+        return self.G
+
         # TODO
 
     def get_num_edges(self):
@@ -19,6 +60,7 @@ class Model:
         Restituisce il numero di Tratte (edges) del grafo
         :return: numero di edges del grafo
         """
+        return self.G.number_of_edges()
         # TODO
 
     def get_num_nodes(self):
@@ -26,6 +68,7 @@ class Model:
         Restituisce il numero di Hub (nodi) del grafo
         :return: numero di nodi del grafo
         """
+        return self.G.number_of_nodes()
         # TODO
 
     def get_all_edges(self):
@@ -33,5 +76,6 @@ class Model:
         Restituisce tutte le Tratte (gli edges) con i corrispondenti pesi
         :return: gli edges del grafo con gli attributi (il weight)
         """
+        return self.G.edges(data=True) # Restituisce una lista di tuple (nodo1, nodo2, dizionario_attributi)
         # TODO
 
